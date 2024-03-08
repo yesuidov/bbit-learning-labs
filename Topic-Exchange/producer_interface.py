76e6e194200c518842760f5f48efe4b59f6368d5
sallyr0823 <chenyan7@illinois.edu>
2024-03-07 20:15:44 -0600

    restore file

--- Topic-Exchange/producer_interface.py
+++ Topic-Exchange/producer_interface.py
@@ -11,31 +11,36 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+import pika,os,sys
 class mqProducerInterface:
     def __init__(self, exchange_name: str) -> None:
         # Save parameters to class variables
-
+        self.exchange_name = exchange_name
+        self.routing_key = sys.argv[1]
+        self.ticker = sys.argv[2]
+        self.price = sys.argv[3]
+        self.sector = sys.argv[4]
         # Call setupRMQConnection
-
-        pass
+        self.setupRMQConnection()
 
     def setupRMQConnection(self) -> None:
         # Set-up Connection to RabbitMQ service
+        con_params = pika.URLParameters(os.environ["AMQP_URL"])
+        self.connection = pika.BlockingConnection(parameters=con_params)
 
         # Establish Channel
-
-        # Create the topic exchange if not already present
-
-        pass
+        self.channel = self.connection.channel()
+        # Create the exchange if not already present
+        self.exchange = self.channel.exchange_declare(exchange=self.exchange_name,exchange_type="topic")
 
     def publishOrder(self, message: str) -> None:
-        # Create Appropiate Topic String
-
-        # Send serialized message or String
-
-        # Print Confirmation
-
-        # Close channel and connection
-
-        pass
+        # Basic Publish to Exchange
+        self.channel.basic_publish(
+        exchange=self.exchange_name,
+        routing_key=self.routing_key,
+        body=message
+        )
+        # Close Channel
+        self.channel.close()
+        self.connection.close()
+        # Close Connection
